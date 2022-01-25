6de481f93a778e7620cc065b75c05b4ef2c7e60c
mbatwc113114 <95271461+mbatwc113114@users.noreply.github.com>
2022-01-25 15:30:30 +0530

    dfdsf

--- js/script.js
+++ js/script.js
@@ -9,5 +9,5 @@ function total_amount(){
     const amount =  Number(price.value)*Number(quantity.value);
     console.log(price.value)
     console.log(amount)
-    total.value = amount;
+    net_price.value = amount;
 }
