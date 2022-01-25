2d748e5028fba299c97189acd04743547aff6039
mbatwc113114 <95271461+mbatwc113114@users.noreply.github.com>
2022-01-25 15:11:53 +0530

    dff

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 date_field = document.getElementById("date")
 
 date_field.valueAsDate = new Date();
+var price = document.getElementById("price")
+var quantity = document.getElementById("quantity")
+var total = document.getElementById("total")
+
+function total_amount(){
+    let amount =  int(price.value)*int(quantity)
+    total.innerHTML = amount
+}
